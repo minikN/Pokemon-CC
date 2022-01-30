20b6ee6a08a5a3ca9f99b7919a8504ed92f6ae06
Demis Balbach <db@minikn.xyz>
2022-01-30 22:46:52 +0100

    CRA: Add mui-image.

---
+++ src/Types/mui-image/index.d.ts
@@ -0,0 +1,48 @@
+declare module 'mui-image' {
+  import React from 'react';
+
+  export interface MuiImageProps {
+    alt?: string;
+    bgColor?: string;
+    className?: string;
+    distance?: string | number;
+    duration?: number;
+    easing?: string;
+    errorIcon?: boolean | React.ReactNode;
+    fit?:
+      | 'contain'
+      | 'cover'
+      | 'fill'
+      | 'none'
+      | 'scale-down'
+      | 'inherit'
+      | 'initial'
+      | 'revert'
+      | 'unset';
+    height?: string | number;
+    iconWrapperClassName?: string;
+    iconWrapperStyle?: any;
+    position?:
+      | 'static'
+      | 'relative'
+      | 'absolute'
+      | 'fixed'
+      | 'sticky'
+      | 'inherit'
+      | 'initial'
+      | 'revert'
+      | 'unset';
+    shift?: 'left' | 'right' | 'top' | 'bottom' | false | null;
+    shiftDuration?: number;
+    showLoading?: boolean | React.ReactNode;
+    src: string;
+    style?: any;
+    width?: string | number;
+    wrapperClassName?: string;
+    wrapperStyle?: any;
+  }
+
+  declare const Image: React.SFC<MuiImageProps>;
+
+  export default Image;
+}
